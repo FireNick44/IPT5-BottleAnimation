60f98df556586fa829ce08b72dcb1dafee336766
FireNick44 <yannic.studer@protonmail.com>
2022-06-19 18:45:00 +0200

    "working" prototype

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 
 
 
-function moveBottle(bootle1, pixelsDown, pixelsRight){
-    let svg = document.getElementById(bootle1);
+function bottlemouseoverLeft(event){
+    var svg = document.getElementById('bottle');
 
-    let x = svg.getAttribute("cx") + pixelsDown;
-    svg.setAttribute("cx", cx);
 
-    let y = svg.getAttribute("y") + pixelsRight;
-    svg.setAttribute("cy", cy);
+    //svg.classList.add('hoverleft');
+
+    //transition: ease-out 200ms;
+    //
+    
+
+    //svg.setAttribute('transition', 'all 2s' )
+    svg.setAttribute('transform', 'rotate(5) translate(240 -20)')
+    var pos_x = event.clientX;
+    var pos_y = event.clientY;
+
+    console.log('X:' + pos_x + ' Y:' + pos_y);
+
+   // svg.style.left =  pos_x+"px";
+    //svg.style.right =  pos_y+"px";
+
+}
+
+function bottleonmouseleaveLeft(event){
+    var svg = document.getElementById('bottle');
+
+
+    svg.setAttribute('transform', 'rotate(0) translate(-30 298)')
 }
