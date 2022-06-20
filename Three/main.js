70a124f0926627b09e8f78db54dfde4348fef654
FireNick44 <yannic.studer@protonmail.com>
2022-06-20 22:57:32 +0200

    LateNight2

--- Three/main.js
+++ Three/main.js
@@ -1,19 +1,32 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+
+console.log('running main');
+main();
 
 function main(){
-  const scene = new THREE.Scene();
 
-  var box = genObject(20,3,3);
-  scene.add(box);
+  //var box = genObject(20,3,3);
+  //scene.add(box);
 
-  var box1 = genObject(3,30,20);
-  box1.translateX(10)
-  scene.add(box1);
+  //var box1 = genObject(300,300,300);
+  //scene.add(box1);
 
-  //var floor = genFloor(10);
-  //scene.add(floor);
+  const loader = new GLTFLoader();
+  loader.load( './model/bottle3D/scene.gltf', ( gltf ) => {
+    gltf.scene.scale.setScalar(2);
+    gltf.scene.translateY(20);
+
+    scene.add( gltf.scene );
+
+  });
+
+  const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+  scene.add( light );
 
   const camera = new THREE.PerspectiveCamera(
     50, 
@@ -21,10 +34,9 @@ function main(){
     1, 
     1000 
   );
-
+  
   camera.position.setZ(30);
-
-
+  
   const renderer = new THREE.WebGLRenderer({
     //select canvas
     canvas: document.getElementById('bg'),
@@ -33,6 +45,8 @@ function main(){
   //set ratio + size
   renderer.setSize( window.innerWidth, window.innerHeight );
   renderer.setPixelRatio( window.devicePixelRatio );
+  //renderer.setClearColor()
+  renderer.setClearColor( 0x222222, 5);
 
   const gridHelper = new THREE.GridHelper(200, 50);
   scene.add(gridHelper);
@@ -46,7 +60,7 @@ function main(){
 function genObject(x, y, z){
   var geo = new THREE.BoxGeometry(x, y, z);
   var mat = new THREE.MeshBasicMaterial({
-    color: 0xffffff
+    color: 0x222222
   });
 
   var mesh = new THREE.Mesh(geo, mat);
@@ -75,21 +89,29 @@ function update(renderer, scene, camera, controls){
   
 }
 
-
-console.log('running main');
-main();
-
+//var rndmBox =  randObj();
+//scene.add(rndmBox);
 
 
+function randObj(){
 
+  var box = genObject( 
+    getRandom(-100, 100),
+    getRandom(-100, 100),
+    getRandom(-100, 100)
+  );
 
+box.translateX(getRandom(-100, 100));
+box.translateY(getRandom(-100, 100));
+box.translateZ(getRandom(-100, 100));
 
+  return box
+}
 
-//const pointLight = new THREE.PointLight(0xffffff);
-//pointLight.position.set(5,5,5);
-
-//scene.add(pointLight);
-
-
-
+function getRandom(min, max){
+  var rndm = Math.floor(Math.random()*max);
+  var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+  
+  return rndm * plusOrMinus;
+}
 
